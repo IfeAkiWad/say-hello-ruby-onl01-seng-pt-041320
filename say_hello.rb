b42a5b82b81a28a77cb1c726e2069404a638d061
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-14 18:37:37 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -6,8 +6,8 @@ def say_hello(name)
 say_hello("Kent Beck")
 
 # Default argument
-def say_hello(name="Ruby programmer")
+def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
   end
 
-say_hello("Ruby programmer")
+say_hello("Ruby Programmer")
